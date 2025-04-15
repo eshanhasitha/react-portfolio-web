a8eba1d0ac18fabc6d6acdf28f3da39b3be5c923
Eshanhasitha <eshanhasitha5@gmail.com>
2025-04-15 10:06:34 +0530

    added avatar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './NavBar.css'
 
+const user = {
+  name: 'Esham Hasitha',
+  imageUrl: 'https://avatars.githubusercontent.com/u/181459091?s=400&u=6ba444a46d0ac4751acd8c50ada2155a3fb7149d&v=4',
+  imageSize: 90,
+};
+
 function NavBar() {
   return (
     <div className='navbar'>  
-      <a href='#h'><img src='n' alt='logo'/></a>
+      <a href='#h'><img 
+      src={user.imageUrl} 
+      alt={'Photo of '+user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize,
+        borderRadius: '50%',
+        margin: '0 10px 0 0',
+      }} />
+      </a>
       <ul className='nav-links'>
         <li ><a className='nav-link' href='#home'>Home</a></li>       
         <li ><a className='nav-link' href='#projects'>Projects</a></li>       
