376fc15277ce38c274bc7b460f917e13a126cec4
Eshanhasitha <eshanhasitha5@gmail.com>
2025-06-01 00:13:25 +0530

    Added Mouse folower Dot.js and Dot.css

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import About from './components/about/About'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Dot from './components/dot/Dot'
 
 
 
 function App() {
   return (
+    
     <div>
+      <Dot/>
       <NavBar/>
       <Profile/>
       <Projects/>
       <About/>
       <Contact/>
       <Footer/>
+      
     </div>
   )
 }
---
+++ src/components/dot/Dot.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react';
+import './Dot.css';
+
+function Dot() {
+  const dotRef = useRef(null);
+
+  useEffect(() => {
+    const dot = dotRef.current;
+    let mouseX = 0,
+      mouseY = 0;
+    let dotX = 0,
+      dotY = 0;
+
+    const handleMouseMove = (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+    };
+
+    const animate = () => {
+      dotX += (mouseX - dotX - 10) * 0.1;
+      dotY += (mouseY - dotY - 10) * 0.1;
+      if (dot) {
+        dot.style.transform = `translate(${dotX}px, ${dotY}px)`;
+      }
+      requestAnimationFrame(animate);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    animate();
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  });
+
+  return <div className="dot" ref={dotRef}></div>;
+}
+
+export default Dot;
